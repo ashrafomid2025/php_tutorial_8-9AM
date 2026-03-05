c8e610e30e2e9333c451823163fbdc166135f94d
ashrafomid2025 <omidmashraf@gmail.com>
2026-03-05 09:10:07 +0430

    static method taught

--- oop.php
+++ oop.php
@@ -1,48 +1,41 @@
 <?php
 
-class Animal{
-   private $name;
-   private $age;
-   
-   public function __construct($nam, $sin){
-      $this->name = $nam;
-      $this->age = $sin;
-   }
-  
-   public function eat(){
-      return "$this->name can eat something";
-   }
-   public function drink(){
-      return "$this->name can drink";
-   }
-   public function setName($name){
+class User{
+   protected $name;
+   public $email;
+
+   public $userType = "user";
+
+   public function __construct($name, $email){
       $this->name = $name;
+      $this->email = $email;
+   
    }
-   public function getName(){
-      return $this->name;
+   // desctruct, clone,
+ 
+
+   public function Message(){
+      return "$this->name sent you a message";
+   }
+
+   public function addFriend(){
+      return "$this->email added you as a friend";
    }
 }
 
-$animalNo1 = new Animal("Cat", 3);
-// echo $animalNo1->getName();
-
-class Human extends Animal{
-   private $degreeOfEducation;
-   private $lastName;
-   public function __construct($name,$age, $degree, $lastName){
-      $this->degreeOfEducation = $degree;
-      $this->lastName = $lastName;
-      parent::__construct($name,$age);
+class AdminUser extends User{
+  public static $level;
+  public function __construct($name, $email, $level){
+      $this->level = $level;
+      parent::__construct($name, $email);
+  }
+    public function Message(){
+      return "$this->name ,admin, sent you a message";
    }
-   // override
-   public function Think(){
-      return "Human can think";
-   }
-
-
+   public static function deleteUser(){
+      echo "One user has been deleted";
+   } 
+   // static 
 }
 
-$person1 =new Human("Ali",23,12,"Ahmadi");
-$person1->setName("Aliullah");
-echo $person1->getName();
-// echo $person1->Think();
+AdminUser::deleteUser();
