1a9c80ec65688b4422f06944d540355cc59a31b3
ashrafomid2025 <omidmashraf@gmail.com>
2026-03-08 08:56:17 +0430

    interface taught

--- oop.php
+++ oop.php
@@ -1,42 +1,40 @@
 <?php
-
-class User{
-   protected $name;
-   public $email;
-
-   public $userType = "user";
-
-   public function __construct($name, $email){
-      $this->name = $name;
-      $this->email = $email;
-   
+abstract class Animal{
+   public function sound(){
+      echo "Animal can make sound";
    }
-   // desctruct, clone,
- 
+  abstract public function move();
+}
 
-   public function Message(){
-      return "$this->name sent you a message";
-   }
-
-   public function addFriend(){
-      return "$this->email added you as a friend";
+class Cat extends Animal{
+   public function move(){
+      echo "Cat move with 4 legs";
    }
 }
 
-class AdminUser extends User{
-  public static $level;
-  public function __construct($name, $email, $level){
-      $this->level = $level;
-      parent::__construct($name, $email);
-  }
-    public function Message(){
-      return "$this->name ,admin, sent you a message";
+// polymorphism
+class Bird extends Animal{
+   public function move(){
+      echo "bird can fly";
    }
-   public static function deleteUser(){
-      echo "One user has been deleted";
-   } 
-   // static 
+}
+// interface, abstract
+
+interface Shape{
+  public function Area();
+  
+   public function ShapeType();
 }
 
-AdminUser::deleteUser();
+class Circle implements Shape{
+   public function Area(){
+      echo 3.14 * 5 * 5;
+   }
+   public function ShapeType()
+   {
+      
+   }
+}
 
+
+// Math, Date
